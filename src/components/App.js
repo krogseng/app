a87c8876f09c7d1618834a8609eb7c1e5f9183e4
Brigitte Huneke <brigittehuneke@gmail.com>
2017-03-24 15:02:41 -0700

    basic components and routing

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import NavBar from './NavBar';
+import MainBody from './MainBody';
+import Footer from './Footer';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+
+class App extends Component {
+    render() {
+        return (
+            < Router >
+                < div>
+                    < Route path='/' component={ NavBar } />
+                    < MainBody />
+                    < Route path='/' component={ Footer }/>
+                </div>
+            </ Router >
+        );
+    }
+}
+
+export default App;
---
+++ src/components/UserMain.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function UserMain(props) {
+    return (
+        <div>
+            User main page, will need to be a class, have title component and body component
+        </div>
+    );
+}
