8451e42c080bba850c4777257dbce2e2c1bf24b8
Brigitte Huneke <brigittehuneke@gmail.com>
2017-03-25 15:04:04 -0700

    added notes for future work

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export default function UserHeader(props) {
+    //logic to determine how date should be displayed (day vs week vs month)
     return (
         <section className='container title'>
             <div>
--- src/components/UserMain.js
+++ src/components/UserMain.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import UserHeader from './UserHeader';
 import UserMoodsDay from './UserMoodsDay';
+import fetcher from '../helpers/fetcher';
 
-export default function UserMain(props) {
-    return (
-        <div>
-            < UserHeader />
-            < UserMoodsDay />
-        </div>
-    );
+export default class UserMain extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    //fetcher for user id?
+
+    render() {
+        return (
+            <div>
+                < UserHeader />
+                < UserMoodsDay />
+            </div>
+        );
+    }
 }
