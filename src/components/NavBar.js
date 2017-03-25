85883ddf56373f12d80cdd5b1c3c98786d570372
Brigitte Huneke <brigittehuneke@gmail.com>
2017-03-25 10:14:14 -0700

    testing styles for the homepage

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 export default function NavBar(props) {
     return (
-        <nav>
-            <ul>
-                <li><Link to='/'>home</Link></li>
-                <li><Link to='/signin'>signin</Link></li>
-                <li><Link to='/allusers'>all users</Link></li>
-                <li><Link to='/user'>user</Link></li>
+        <nav className='container'>
+            <ul className='twelve columns'>
+                <li className='navbar-item'><Link to='/'>home</Link></li>
+                <li className='navbar-item'><Link to='/signin'>signin</Link></li>
+                <li className='navbar-item'><Link to='/allusers'>all users</Link></li>
+                <li className='navbar-item'><Link to='/user'>user</Link></li>
             </ul>
         </nav>
     );
