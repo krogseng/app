ae1917e4deb2607f04fe3b3b2111a8cf221ead70
Brigitte Huneke <brigittehuneke@gmail.com>
2017-03-25 10:14:44 -0700

    added color boxes to the user page

---
+++ src/components/UserHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function UserHeader(props) {
+    return (
+        <section className='container title'>
+            <div>
+                <h1>USERNAME</h1>
+                <h5>Date</h5>
+            </div>
+        </section>
+    );
+}
--- src/components/UserMain.js
+++ src/components/UserMain.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import UserHeader from './UserHeader';
+import UserMoodsDay from './UserMoodsDay';
 
 export default function UserMain(props) {
     return (
         <div>
+            < UserHeader />
+            < UserMoodsDay />
             User main page, will need to be a class, have title component and body component
         </div>
     );
