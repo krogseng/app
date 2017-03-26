e0115576fac1f6e7cd2f5d59cd7de156b5c4b439
Brigitte Huneke <brigittehuneke@gmail.com>
2017-03-26 16:05:26 -0700

    added functionality to handle token

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import NavBar from './NavBar';
 import MainBody from './MainBody';
 import Footer from './Footer';
+import fetcher from '../helpers/fetcher';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 
@@ -9,9 +10,32 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            isSignedIn: false,
+            token: null,
         };
+        this.hydrateAuth = this.hydrateAuth.bind(this);
     }
+
+
+    hydrateAuth() {
+        const token = localStorage.getItem('token');
+        fetcher({
+            path: '/auth/verify',
+            method: 'GET',
+            token: token,
+        })
+        .then(res => res.json())
+        .then(json => {
+            if(json.valid) {
+                this.setState({
+                    isSignedIn: true,
+                    token: token,
+                });
+            }
+        });
+
+    }
+
     //fetch for colors
     render() {
         return (
