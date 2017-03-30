43624cbf23ec367c56defbf5306ee971564a2911
Clarefollett <chlaw101@gmail.com>
2017-03-30 14:52:21 -0700

    got the chart displaying the colors and values, now working on having the moods display

--- src/components/UserMonthChart.js
+++ src/components/UserMonthChart.js
@@ -1,11 +1,66 @@
 import React from 'react';
+import RC2 from 'react-chartjs2';
+
+
+// class Pie extends Component {
+//   render() {
+//     return (
+//       <RC2 data={data} type='pie' />
+//     );
+//   }
+// }
 
 export default function UserMonthChart(props) {
+    const cfCount = count(props.monthColors)
+    const lkLabels = Object.keys(cfCount)
+    
+    const data = {
+    labels: lkLabels,
+    datasets: [{
+        data: values(cfCount),
+        backgroundColor: lkLabels
+        // hoverBackgroundColor: [
+        // '#FF6384',
+        // '#36A2EB',
+        // '#FFCE56'
+        // ]
+    }]
+};
+
+
+    function count(monthColors) {
+        const colorCount = {}
+        monthColors.forEach((monthColor) => {
+            let indexColor = monthColor.color.hexColor
+            if (colorCount[indexColor]) colorCount[indexColor] = colorCount[indexColor] + 1
+            else colorCount[indexColor] = 1;
+        })
+        return colorCount;
+    }
+
+    function values (monthColorsCount) {
+        const colorValues = []
+        Object.keys(monthColorsCount).forEach((key) => {
+            colorValues.push(monthColorsCount[key])
+        })
+        return colorValues;
+    }
+
+    function mood () {
+        
+    }
+
+    
+    
+
+    
+
+    
     return (
         <div className='container'>
             <div className='10 columns' style={{ backgroundColor: '#E6E6E6', height: 500 }}>
                 <div>
-                     Insert Chart Here
+                     <RC2 data={data} type='pie' />
                 </div>
             </div>
         </div>
