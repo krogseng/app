22fe128a9f8f1f6732344eebbd6a461ee00892f9
Brigitte Huneke <brigittehuneke@gmail.com>
2017-03-30 15:16:06 -0700

    handling merge and fixed linting errors

--- src/components/UserMonthChart.js
+++ src/components/UserMonthChart.js
@@ -11,38 +11,38 @@ import RC2 from 'react-chartjs2';
 // }
 
 export default function UserMonthChart(props) {
-    const cfCount = count(props.monthColors)
-    const lkLabels = Object.keys(cfCount)
+    const cfCount = count(props.monthColors);
+    const lkLabels = Object.keys(cfCount);
     
     const data = {
-    labels: lkLabels,
-    datasets: [{
-        data: values(cfCount),
-        backgroundColor: lkLabels
-        // hoverBackgroundColor: [
-        // '#FF6384',
-        // '#36A2EB',
-        // '#FFCE56'
-        // ]
-    }]
-};
+        labels: lkLabels,
+        datasets: [{
+            data: values(cfCount),
+            backgroundColor: lkLabels
+            // hoverBackgroundColor: [
+            // '#FF6384',
+            // '#36A2EB',
+            // '#FFCE56'
+            // ]
+        }]
+    };
 
 
     function count(monthColors) {
-        const colorCount = {}
+        const colorCount = {};
         monthColors.forEach((monthColor) => {
-            let indexColor = monthColor.color.hexColor
-            if (colorCount[indexColor]) colorCount[indexColor] = colorCount[indexColor] + 1
+            let indexColor = monthColor.color.hexColor;
+            if (colorCount[indexColor]) colorCount[indexColor] = colorCount[indexColor] + 1;
             else colorCount[indexColor] = 1;
-        })
+        });
         return colorCount;
     }
 
     function values (monthColorsCount) {
-        const colorValues = []
+        const colorValues = [];
         Object.keys(monthColorsCount).forEach((key) => {
             colorValues.push(monthColorsCount[key])
-        })
+        });
         return colorValues;
     }
 
